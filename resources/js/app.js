36df7b326c27b6f20f3c267bc3500bd46e6e5b43
Erdin IDN <erdin@idnplay.com>
2022-08-10 22:25:58 +0700

    Add tailwind, vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue/dist/vue.esm-bundler";
+import Rolling from "./components/Rolling.vue";
+
+var app = createApp({
+    components: {
+        Rolling,
+    },
+    mounted() {
+        console.log("Its working");
+    },
+});
+
+app.mount("#app");
---
+++ resources/js/components/Rolling.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <p class="text-white text-3xl font-bold">{{ number }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "Rolling",
+    mounted() {
+        var interval = setInterval(() => {
+            this.number = this.generateString(2);
+        }, 50);
+
+        setTimeout(() => {
+            clearInterval(interval);
+            this.number = "24";
+        }, 5000);
+    },
+    data() {
+        return {
+            number: "00",
+        };
+    },
+    methods: {
+        generateString(len) {
+            let text = " ";
+            let chars = "0123456789";
+
+            for (let i = 0; i < len; i++) {
+                text += chars.charAt(Math.floor(Math.random() * chars.length));
+            }
+
+            return text;
+        },
+    },
+};
+</script>
